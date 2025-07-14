ef688b6010fbe23bb86f02ea076cdfb3f4f4fa5e
Kedaar Rentachintala <kedaarnr@berkeley.edu>
2025-07-14 14:01:50 -0700

    Add shebang, imports, and logging setup for Twitter scraper service

---
+++ twitter_scraper_service.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+import pandas as pd
+from typing import Dict, List, Optional, Any
+from dataclasses import dataclass, asdict
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import time
+import os
+import re
+from datetime import datetime, timedelta
+import requests
+from io import BytesIO
+from urllib.parse import urljoin, urlparse
+
+try:
+    from bs4 import BeautifulSoup
+    BS4_AVAILABLE = True
+except ImportError:
+    BS4_AVAILABLE = False
+
+try:
+    import snscrape.modules.twitter as sntwitter
+    SNSCRAPE_AVAILABLE = True
+except ImportError:
+    SNSCRAPE_AVAILABLE = False
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
